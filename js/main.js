083e68fc7e0a846ef572412e0a412b7045e32f55
Pratik Chudal <pratikchudal145@gmail.com>
2025-07-08 08:12:32 +0545

    Creating the navbar and header section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenu = document.getElementById('mobile-menu');
 
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
-});
